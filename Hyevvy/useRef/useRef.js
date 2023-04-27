70188d5dc02dcb7588a7d49c87fc90cf34ef23c8
hyevvy <hyeg0@naver.com>
2023-04-27 19:25:55 +0900

    jsx

--- Hyevvy/useRef/useRef.js
+++ Hyevvy/useRef/useRef.js
@@ -1,14 +1,7 @@
-``` js
-    function useRef(initialValue){
-        var dispatcher = resolveDispatcher();
-        return dispatcher.useRef(initialValue);
-    }
-```;
-
 ```js
     //바닐라 JS
     const myDiv = document.querySelector('.my-div');
-    myDiv.addEcentListener('click', callbackFn);
+    myDiv.addEventListener('click', callbackFn);
 
     //리액트
     const myDiv = useRef(null);
@@ -16,4 +9,4 @@
     myDiv.current.addEventListener('click', callbackFn);
 
     document.querySelector('.my-div'); 이 반환하는 값이 myRef.current에 똑같이 담겨 있음!
-    ```;
+```;
