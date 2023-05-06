e5270655852e0e0bbb449887c267a9a5da29a61e
Yoon-Hae-Min <yunhatmi@naver.com>
2023-05-07 01:03:15 +0900

    useEffect 실행 순서 조절 및 child 컴포넌트 적용

---
+++ 윤해민/FixuseEffect/dist/Component.js
@@ -0,0 +1,5 @@
+import React from "./React.js";
+const Component = () => {
+  return /*#__PURE__*/React.createElement("div", null, "\uCEF4\uD3EC\uB10C\uD2B8 \uC785\uB2C8\uB2E4.");
+};
+export default Component;
---
+++ 윤해민/FixuseEffect/src/Component.js
@@ -0,0 +1,7 @@
+import React from "./React.js";
+
+const Component = () => {
+  return <div>컴포넌트 입니다.</div>;
+};
+
+export default Component;
